ac38f1ae22ba1dedfd19b0d615d5120633a8eb34
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-10 18:21:38 -0500

    feat(kotlin): implement session factory synced with mutiny/stage impl

--- hibernate-reactive-core-kt/src/main/kotlin/org/hibernate/reactive/coroutines/impl/CoroutinesSessionFactoryImpl.kt
+++ hibernate-reactive-core-kt/src/main/kotlin/org/hibernate/reactive/coroutines/impl/CoroutinesSessionFactoryImpl.kt
@@ -19,6 +19,7 @@ import org.hibernate.reactive.common.InternalStateAssertions.assertUseOnEventLoo
 import org.hibernate.reactive.common.spi.Implementor
 import org.hibernate.reactive.context.Context
 import org.hibernate.reactive.context.impl.BaseKey
+import org.hibernate.reactive.context.impl.MultitenantKey
 import org.hibernate.reactive.coroutines.Coroutines
 import org.hibernate.reactive.coroutines.DelicateHibernateReactiveCoroutineApi
 import org.hibernate.reactive.coroutines.ExperimentalHibernateReactiveCoroutineApi
@@ -27,6 +28,8 @@ import org.hibernate.reactive.logging.impl.Log
 import org.hibernate.reactive.logging.impl.LoggerFactory
 import org.hibernate.reactive.pool.ReactiveConnection
 import org.hibernate.reactive.pool.ReactiveConnectionPool
+import org.hibernate.reactive.session.impl.ReactiveSessionImpl
+import org.hibernate.reactive.session.impl.ReactiveStatelessSessionImpl
 import org.hibernate.service.ServiceRegistry
 import org.hibernate.stat.Statistics
 import java.lang.invoke.MethodHandles
@@ -58,9 +61,10 @@ class CoroutinesSessionFactoryImpl(private val delegate: SessionFactoryImpl) :
             "No existing open Coroutines.Session was found in the current Vert.x context for current tenant '%s': opening a new instance"
         private const val OPENING_NEW_STATELESS_SESSION =
             "No existing open Coroutines.StatelessSession was found in the current Vert.x context: opening a new instance"
-    }
 
-    private val log: Log = LoggerFactory.make(Log::class.java, MethodHandles.lookup())
+        @JvmStatic
+        private val log: Log = LoggerFactory.make(Log::class.java, MethodHandles.lookup())
+    }
 
     private val context: Context = delegate.serviceRegistry.requireService(Context::class.java)
 
@@ -74,106 +78,125 @@ class CoroutinesSessionFactoryImpl(private val delegate: SessionFactoryImpl) :
     private val contextKeyForStatelessSession: BaseKey<Coroutines.StatelessSession> =
         BaseKey(Coroutines.StatelessSession::class.java, delegate.uuid)
 
-    // session
-    override suspend fun openSession(): Coroutines.Session = TODO(
-        """
-            val options = options(tenantId, withTenant)
-            val tenantIdentifier = if (withTenant) tenantId else options.tenantIdentifier
-            val reactiveConnection = connection(tenantIdentifier)
-            val session = create(reactiveConnection) {
-                // THIS is core of problem, session impl save the associatedWorkThread and verify it,
-                // coroutines don't guarantee the thread
-                ReactiveSessionImpl(delegate, options, reactiveConnection)
-            }
-            CoroutinesSessionImpl(session, this),
-        """.trimMargin(),
-    )
+    // create session
+    override fun createSession(): Coroutines.Session = createSession(getTenantIdentifier(options()))
 
-    override suspend fun openSession(tenantId: String?): Coroutines.Session = TODO()
+    override fun createSession(tenantId: String?): Coroutines.Session {
+        // Why use options and not pass tenantId ??
+        val options = options()
+        val sessionImpl = ReactiveSessionImpl(delegate, options, connectionPool.getProxyConnection(tenantId))
+        return CoroutinesSessionImpl(sessionImpl)
+    }
 
-    // stateless
-    override suspend fun openStatelessSession(): Coroutines.StatelessSession = TODO(
-        """
-            val options = options(tenantId, withTenant)
-            val tenantIdentifier = if (withTenant) tenantId else options.tenantIdentifier
-            val reactiveConnection = connection(tenantIdentifier)
-            val session = create(reactiveConnection) {
-                // May be converted a problem because is a task check the thread
-                ReactiveStatelessSessionImpl(delegate, options, reactiveConnection)
-            }
-            CoroutinesStatelessSessionImpl(session, this)
-        """.trimIndent(),
-    )
+    // create stateless session
+    override fun createStatelessSession(): Coroutines.StatelessSession =
+        createStatelessSession(getTenantIdentifier(options()))
 
-    override suspend fun openStatelessSession(tenantId: String?): Coroutines.StatelessSession = TODO()
+    override fun createStatelessSession(tenantId: String?): Coroutines.StatelessSession {
+        val options = options()
+        val sessionImpl = ReactiveStatelessSessionImpl(delegate, options, connectionPool.getProxyConnection(tenantId))
+        return CoroutinesStatelessSessionImpl(sessionImpl)
+    }
+
+    // open session
+    override suspend fun openSession(): Coroutines.Session {
+        val options = options()
+        val reactiveConnection = connection(getTenantIdentifier(options))
+        val session = create(reactiveConnection) {
+            // THIS is core of problem, session impl save the associatedWorkThread and verify it,
+            // coroutines don't guarantee the thread
+            ReactiveSessionImpl(delegate, options, reactiveConnection)
+        }
+        return CoroutinesSessionImpl(session)
+    }
+
+    override suspend fun openSession(tenantId: String?): Coroutines.Session {
+        val connection = connection(tenantId)
+        val session = create(connection) {
+            // THIS is core of problem, session impl save the associatedWorkThread and verify it,
+            // coroutines don't guarantee the thread
+            ReactiveSessionImpl(delegate, options(tenantId), connection)
+        }
+        return CoroutinesSessionImpl(session)
+    }
+
+    // open stateless
+    override suspend fun openStatelessSession(): Coroutines.StatelessSession {
+        val options = options()
+        val reactiveConnection = connection(getTenantIdentifier(options))
+        val session = create(reactiveConnection) {
+            // May be converted a problem because is in to-do check the thread
+            ReactiveStatelessSessionImpl(delegate, options, reactiveConnection)
+        }
+        return CoroutinesStatelessSessionImpl(session)
+    }
+
+    override suspend fun openStatelessSession(tenantId: String?): Coroutines.StatelessSession {
+        val connection = connection(tenantId)
+        val session = create(connection) {
+            ReactiveStatelessSessionImpl(delegate, options(tenantId), connection)
+        }
+        return CoroutinesStatelessSessionImpl(session)
+    }
 
     // with session
-    override suspend fun <T> withSession(work: suspend (Coroutines.Session) -> T): T = TODO(
-        """
-            val current = getCurrentSession()
-            return if (current != null && current.isOpen()) {
-                log.debug(REUSING_SESSION)
-                // Resume the work in the hibernate context because
-                // use of the reactive Session from a different Thread than the one which was used to open the reactive Session
-                // See it on InternalStateAssertions.assertCurrentThreadMatches, used on ReactiveSessionImpl
-                // Resume the session with his dispatcher
-                withActiveSession(current, work, contextKeyForSession)
-            } else {
-                log.debug(OPENING_NEW_SESSION)
-                val session = openSession()
-                withSession(session, work, contextKeyForSession)
-            }
-        """.trimIndent(),
-    )
+    override suspend fun <T> withSession(work: suspend (Coroutines.Session) -> T): T {
+        val current = getCurrentSession()
+        return if (current != null && current.isOpen()) {
+            log.debug(REUSING_SESSION)
+            // use of the reactive Session from a different Thread than the one which was used to open the reactive Session
+            // See it on InternalStateAssertions.assertCurrentThreadMatches, used on ReactiveSessionImpl
+            work(current)
+        } else {
+            log.debug(OPENING_NEW_SESSION)
+            val session = openSession()
+            withSession(session, work, contextKeyForSession)
+        }
+    }
 
-    override suspend fun <T> withSession(tenantId: String, work: suspend (Coroutines.Session) -> T): T = TODO(
-        """
-            val key: Context.Key<Coroutines.Session> = MultitenantKey(contextKeyForSession, tenantId)
-            val current: Coroutines.Session? = context[key]
-            return if (current != null && current.isOpen()) {
-                log.debugf(REUSING_TENANT_SESSION, tenantId)
-                withActiveSession(current, work, key)
-            } else {
-                log.debugf(OPENING_NEW_TENANT_SESSION, tenantId)
-                val session = openSession(tenantId)
-                withSession(session, work, key)
-            }
-        """.trimIndent(),
-    )
+    override suspend fun <T> withSession(tenantId: String, work: suspend (Coroutines.Session) -> T): T {
+        val key: Context.Key<Coroutines.Session> = MultitenantKey(contextKeyForSession, tenantId)
+        val current: Coroutines.Session? = context[key]
+        return if (current != null && current.isOpen()) {
+            log.debugf(REUSING_TENANT_SESSION, tenantId)
+            work(current)
+        } else {
+            log.debugf(OPENING_NEW_TENANT_SESSION, tenantId)
+            val session = openSession(tenantId)
+            withSession(session, work, key)
+        }
+    }
 
     // with stateless
-    override suspend fun <T> withStatelessSession(work: suspend (Coroutines.StatelessSession) -> T): T = TODO(
-        """
-            val current = getCurrentStatelessSession()
-            return if (current != null && current.isOpen()) {
-                log.debug(REUSING_STATELESS_SESSION)
-                withActiveSession(current, work, contextKeyForStatelessSession)
-            } else {
-                log.debug(OPENING_NEW_STATELESS_SESSION)
-                val statelessSession = openStatelessSession()
-                withSession(statelessSession, work, contextKeyForStatelessSession)
-            }
-        """.trimIndent(),
-    )
+    override suspend fun <T> withStatelessSession(work: suspend (Coroutines.StatelessSession) -> T): T {
+        val current = getCurrentStatelessSession()
+        return if (current != null && current.isOpen()) {
+            log.debug(REUSING_STATELESS_SESSION)
+            work(current)
+        } else {
+            log.debug(OPENING_NEW_STATELESS_SESSION)
+            val statelessSession = openStatelessSession()
+            withSession(statelessSession, work, contextKeyForStatelessSession)
+        }
+    }
 
     override suspend fun <T> withStatelessSession(
         tenantId: String?,
         work: suspend (Coroutines.StatelessSession) -> T,
-    ): T = TODO(
-        """
-            val key: Context.Key<Coroutines.StatelessSession> = MultitenantKey(contextKeyForStatelessSession, tenantId)
-            val current: Coroutines.StatelessSession? = context[key]
-            return if (current != null && current.isOpen()) {
-                log.debugf(REUSING_TENANT_STATELESS_SESSION, tenantId)
-                withActiveSession(current, work, key)
-            } else {
-                log.debugf(OPENING_NEW_TENANT_STATELESS_SESSION, tenantId)
-                val statelessSession = openStatelessSession(tenantId)
-                withSession(statelessSession, work, key)
-            }
-        """.trimIndent(),
-    )
+    ): T {
+        val key: Context.Key<Coroutines.StatelessSession> = MultitenantKey(contextKeyForStatelessSession, tenantId)
+        val current: Coroutines.StatelessSession? = context[key]
+        return if (current != null && current.isOpen()) {
+            log.debugf(REUSING_TENANT_STATELESS_SESSION, tenantId)
+            work(current)
+        } else {
+            log.debugf(OPENING_NEW_TENANT_STATELESS_SESSION, tenantId)
+            val statelessSession = openStatelessSession(tenantId)
+            withSession(statelessSession, work, key)
+        }
+    }
 
+    // delegate and getters property
     override fun getCriteriaBuilder(): HibernateCriteriaBuilder = delegate.criteriaBuilder
 
     override fun getMetamodel(): Metamodel? = delegate.metamodel
@@ -199,6 +222,13 @@ class CoroutinesSessionFactoryImpl(private val delegate: SessionFactoryImpl) :
     override fun getContext(): Context = context
 
     // private helpers
+    private fun getTenantIdentifier(options: SessionCreationOptions): String? =
+        if (options.tenantIdentifierValue == null) {
+            null
+        } else {
+            delegate.tenantIdentifierJavaType.toString(options.tenantIdentifierValue)
+        }
+
     private fun options(): SessionCreationOptions = object : SessionBuilderImpl(delegate) {
         override fun createSession(): SessionImplementor = SessionImpl(delegate, this)
     }
@@ -208,7 +238,7 @@ class CoroutinesSessionFactoryImpl(private val delegate: SessionFactoryImpl) :
         override fun getTenantIdentifierValue(): Any? = tenantId
     }
 
-    private suspend fun connection(tenantId: String?): ReactiveConnection {
+    private suspend inline fun connection(tenantId: String?): ReactiveConnection {
         assertUseOnEventLoop()
         // TODO this is await of kotlinx.coroutines.jvm check if apply
         return if (tenantId == null) {
@@ -218,7 +248,7 @@ class CoroutinesSessionFactoryImpl(private val delegate: SessionFactoryImpl) :
         }
     }
 
-    private final suspend fun <S> create(connection: ReactiveConnection, supplier: () -> S): S {
+    private final suspend inline fun <S> create(connection: ReactiveConnection, supplier: () -> S): S {
         contract { callsInPlace(supplier, InvocationKind.EXACTLY_ONCE) }
         return try {
             // ReactiveSessionImpl and ReactiveStatelessSessionImpl need to be called in event loop
@@ -232,39 +262,13 @@ class CoroutinesSessionFactoryImpl(private val delegate: SessionFactoryImpl) :
     }
 
     /** Put in context the session and delegate await to close and remove from the context */
-    private final suspend fun <S : Coroutines.Closeable, T> withSession(
+    private final suspend inline fun <S : Coroutines.Closeable, T> withSession(
         session: S,
         work: suspend (S) -> T,
         contextKey: Context.Key<S>,
     ): T {
         contract { callsInPlace(work, InvocationKind.EXACTLY_ONCE) }
         context[contextKey] = session
-
-        return try {
-            work(session)
-        } finally {
-            context.remove(contextKey)
-            try {
-                session.close()
-            } catch (_: Throwable) {
-                // TODO need to check if is safe catch CancellationException of coroutines
-                // currentCoroutineContext().ensureActive()
-                // Only throw the original exception in case an error occurs while closing the session
-            }
-        }
-    }
-
-    /**
-     * Prevent resource leak and blocking the event loop with an active session.
-     * This method doesn't put in the context the key, only remove it.
-     */
-    private final suspend fun <S : Coroutines.Closeable, T> withActiveSession(
-        session: S,
-        work: suspend (S) -> T,
-        contextKey: Context.Key<S>,
-    ): T {
-        contract { callsInPlace(work, InvocationKind.EXACTLY_ONCE) }
-        checkNotNull(context[contextKey])
 
         return try {
             work(session)
