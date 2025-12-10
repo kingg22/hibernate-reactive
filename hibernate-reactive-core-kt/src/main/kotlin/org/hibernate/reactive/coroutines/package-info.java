061527cdfdd0f9e64ed96333b88aa898844b2a7c
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-10 17:17:45 -0500

    feat(kotlin): add package info for coroutines

---
+++ hibernate-reactive-core-kt/src/main/kotlin/org/hibernate/reactive/coroutines/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+/**
+ * An API for Hibernate Reactive where non-blocking operations are represented by suspend coroutines method.
+ *
+ * @see org.hibernate.reactive.coroutines.Coroutines
+ */
+package org.hibernate.reactive.coroutines;
